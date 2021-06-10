a2501fe4ddcf4e7d948967bf0fee0f3da2606188
toru.takahara <taka635765@gmail.com>
2021-06-10 22:12:23 +0900

    Fix;

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+ 
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCkV7Eo9f9Qsierc07_yjXD_tWYVeK9SKI",
+    authDomain: "shop-review-app-eaa55.firebaseapp.com",
+    projectId: "shop-review-app-eaa55",
+    storageBucket: "shop-review-app-eaa55.appspot.com",
+    messagingSenderId: "978963790149",
+    appId: "1:978963790149:web:4e562afe279178e9d8a62c",
+    measurementId: "G-PPZ1ZHXVNC",
+  })
+}
+ 
+export default firebase
